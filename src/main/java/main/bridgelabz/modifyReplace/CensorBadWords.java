efbef8242877738beb5871efa942bfd46bccbb68
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-04-02 19:58:45 +0530

    [Sanya] Add: Censor Bad Words in a Sentence

---
+++ src/main/java/main/bridgelabz/modifyReplace/CensorBadWords.java
@@ -0,0 +1,27 @@
+package main.bridgelabz.modifyReplace;
+
+import java.util.*;
+public class CensorBadWords {
+    public static void main(String[] args){
+        List <String> badwords= Arrays.asList("damn", "stupid");
+
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Enter the sentence: ");
+        String s= sc.nextLine();
+
+        String regex="\\b(" + String.join("|", badwords) + ")\\b";
+
+        String result = s.replaceAll(regex, "****");
+
+        System.out.println("Censored Sentence:");
+        System.out.println(result);
+    }
+}
+/*
+Enter the sentence:
+This is a damn bad example with some stupid words
+Censored Sentence:
+This is a **** bad example with some **** words
+
+ */
